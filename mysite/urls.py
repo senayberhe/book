eff3777f08223ef2fc9fcfcf4778c29704d8879f
senayberhe <tsionberhe@gmail.com>
2022-04-06 13:00:54 -0700

    created authetications

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #user management 
+    path('accounts/', include('django.contrib.auth.urls')),
+    #local apps 
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
