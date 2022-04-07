4640d9046a2c0d9e5700df1f6d77b24e1cd46fb8
senayberhe <tsionberhe@gmail.com>
2022-04-07 14:32:28 -0700

    updated images

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static 
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,9 +7,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #user management 
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
     #local apps 
     path('', include('allauth.urls')),
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
