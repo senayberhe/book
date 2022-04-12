828c3827a16a034421d47a9d6fe02932ec0eb721
senayberhe <tsionberhe@gmail.com>
2022-04-12 10:38:18 -0700

    updated settings files

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'senaybookstore.herokuapp.com/']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'senaybookstore.herokuapp.com']
 
 
 # Application definition
